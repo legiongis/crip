7d457c98c09a761753fd58bf2e4168e4c3685eea
adam cox <mr.adamcox@gmail.com>
2015-04-13 16:05:11 -0500

    updated database variables in settings.py

--- crip/settings.py
+++ crip/settings.py
@@ -4,9 +4,9 @@ from arches_hip.settings import *
 
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 PACKAGE_NAME = PACKAGE_ROOT.split(os.sep)[-1]
-DATABASES['default']['NAME'] = 'template_postgis_21'
+DATABASES['default']['NAME'] = 'arches_%s' % (PACKAGE_NAME)
+DATABASES['default']['POSTGIS_TEMPLATE'] = 'template_postgis_21'
 ROOT_URLCONF = '%s.urls' % (PACKAGE_NAME)
-
 INSTALLED_APPS = INSTALLED_APPS + (PACKAGE_NAME,)
 STATICFILES_DIRS = (os.path.join(PACKAGE_ROOT, 'media'),) + STATICFILES_DIRS
 TEMPLATE_DIRS = (os.path.join(PACKAGE_ROOT, 'templates'),os.path.join(PACKAGE_ROOT, 'templatetags')) + TEMPLATE_DIRS
