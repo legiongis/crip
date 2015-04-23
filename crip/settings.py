753758995cb474716273c14074e909a103500dfb
mradamcox <mr.adamcox@gmail.com>
2015-04-23 13:45:59 -0500

    update settings and import statement in resource.py

--- crip/settings.py
+++ crip/settings.py
@@ -12,7 +12,6 @@ TEMPLATE_DIRS = (os.path.join(PACKAGE_ROOT, 'templates'),os.path.join(PACKAGE_RO
 
 RESOURCE_MODEL = {'default': 'crip.models.resource.Resource'}
 
-
 DEFAULT_MAP_X = -10362191
 DEFAULT_MAP_Y = 3731083 
 DEFAULT_MAP_ZOOM = 10
@@ -21,47 +20,123 @@ MAP_MAX_ZOOM = 20
 # MAP_EXTENT = '-13228037.69691764,3981296.0184014924,-13123624.71628009,4080358.407059081'
 
 
-# def RESOURCE_TYPE_CONFIGS():
-#     return { 
-#         'HERITAGE_RESOURCE.E18': {
-#             'resourcetypeid': 'HERITAGE_RESOURCE.E18',
-#             'name': _('Historic Resource'),
-#             'icon_class': 'fa fa-university',
-#             'default_page': 'summary',
-#             'default_description': 'No description available',
-#             'description_node': _('DESCRIPTION.E62'),
-#             'categories': [_('Resource')],
-#             'has_layer': True,
-#             'on_map': False,
-#             'marker_color': '#fa6003',
-#             'stroke_color': '#fb8c49',
-#             'fill_color': '#ffc29e',
-#             'primary_name_lookup': {
-#                 'entity_type': 'NAME.E41',
-#                 'lookup_value': 'Primary'
-#             },
-#             'sort_order': 1
-#         },
-#         'HERITAGE_RESOURCE_GROUP.E27': {
-#             'resourcetypeid': 'HERITAGE_RESOURCE_GROUP.E27',
-#             'name': _('Historic District'),
-#             'icon_class': 'fa fa-th',
-#             'default_page': 'summary',
-#             'default_description': 'No description available',
-#             'description_node': _('REASONS.E62'),
-#             'categories': [_('Resource')],
-#             'has_layer': True,
-#             'on_map': False,
-#             'marker_color': '#FFC53D',
-#             'stroke_color': '#d9b562',
-#             'fill_color': '#eedbad',
-#             'primary_name_lookup': {
-#                 'entity_type': 'NAME.E41',
-#                 'lookup_value': 'Primary'
-#             },
-#             'sort_order': 2
-#         }
-#     }
+def RESOURCE_TYPE_CONFIGS():
+    return {
+        'HERITAGE_RESOURCE.E18': {
+            'resourcetypeid': 'HERITAGE_RESOURCE.E18',
+            'name': _('Historic Resource'),
+            'icon_class': 'fa fa-university',
+            'default_page': 'summary',
+            'default_description': 'No description available',
+            'description_node': _('DESCRIPTION.E62'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#fa6003',
+            'stroke_color': '#fb8c49',
+            'fill_color': '#ffc29e',
+            'primary_name_lookup': {
+                'entity_type': 'NAME.E41',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 1
+        },
+        'HERITAGE_RESOURCE_GROUP.E27': {
+            'resourcetypeid': 'HERITAGE_RESOURCE_GROUP.E27',
+            'name': _('Historic District'),
+            'icon_class': 'fa fa-th',
+            'default_page': 'summary',
+            'default_description': 'No description available',
+            'description_node': _('REASONS.E62'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#FFC53D',
+            'stroke_color': '#d9b562',
+            'fill_color': '#eedbad',
+            'primary_name_lookup': {
+                'entity_type': 'NAME.E41',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 2
+        },
+        'ACTIVITY.E7': {
+            'resourcetypeid': 'ACTIVITY.E7',
+            'name': _('Activity'),
+            'icon_class': 'fa fa-tasks',
+            'default_page': 'activity-summary',
+            'default_description': 'No description available',
+            'description_node': _('INSERT RESOURCE DESCRIPTION NODE HERE'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#6DC3FC',
+            'stroke_color': '#88bde0',
+            'fill_color': '#afcce1',
+            'primary_name_lookup': {
+                'entity_type': 'NAME.E41',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 3
+        },
+        'HISTORICAL_EVENT.E5':{
+            'resourcetypeid': 'HISTORICAL_EVENT.E5',
+            'name': _('Historic Event'),
+            'icon_class': 'fa fa-calendar',
+            'default_page': 'historical-event-summary',
+            'default_description': 'No description available',
+            'description_node': _('INSERT RESOURCE DESCRIPTION NODE HERE'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#4EBF41',
+            'stroke_color': '#61a659',
+            'fill_color': '#c2d8bf',
+            'primary_name_lookup': {
+                'entity_type': 'NAME.E41',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 4
+        },
+        'ACTOR.E39': {
+            'resourcetypeid': 'ACTOR.E39',
+            'name': _('Person/Organization'),
+            'icon_class': 'fa fa-group',
+            'default_page': 'actor-summary',
+            'default_description': 'No description available',
+            'description_node': _('INSERT RESOURCE DESCRIPTION NODE HERE'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#a44b0f',
+            'stroke_color': '#a7673d',
+            'fill_color': '#c8b2a3',
+            'primary_name_lookup': {
+                'entity_type': 'ACTOR_APPELLATION.E82',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 5
+        },
+        'INFORMATION_RESOURCE.E73': {
+            'resourcetypeid': 'INFORMATION_RESOURCE.E73',
+            'name': _('Information Resource'),
+            'icon_class': 'fa fa-file-text-o',
+            'default_page': 'information-resource-summary',
+            'default_description': 'No description available',
+            'description_node': _('INSERT RESOURCE DESCRIPTION NODE HERE'),
+            'categories': [_('Resource')],
+            'has_layer': True,
+            'on_map': False,
+            'marker_color': '#8D45F8',
+            'stroke_color': '#9367d5',
+            'fill_color': '#c3b5d8',
+            'primary_name_lookup': {
+                'entity_type': 'TITLE.E41',
+                'lookup_value': 'Primary'
+            },
+            'sort_order': 6
+        }
+    }
 
 GEOCODING_PROVIDER = 'arches.app.utils.bing_geocoder'
 BING_KEY = "AlBL1IuDdvcboF7cWajeIchjXkr7j2sA_oTYElH_ooYyx6115-GoenKljGJfDopa"
