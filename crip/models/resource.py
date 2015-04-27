0f9442820fd300d52ad0238de6af610c7e76ce41
mradamcox <mr.adamcox@gmail.com>
2015-04-27 10:58:09 -0500

    delete second File Upload form reference

--- crip/models/resource.py
+++ crip/models/resource.py
@@ -111,9 +111,6 @@ class Resource(ArchesResource):
                 forms.DescriptionForm.get_info(),
                 forms.FileUploadForm.get_info()
             ]
-            #description_group['forms'].append(forms.FileUploadForm.get_info())
-
-            
 
         if self.entityid != '':
             self.form_groups.append({
