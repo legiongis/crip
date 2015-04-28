29dd098c3a3beadf1e11d9a83c31337593ef6748
mradamcox <mr.adamcox@gmail.com>
2015-04-28 17:09:10 -0500

    add more messages

--- crip/media/js/views/forms/sections/branch-list.js
+++ crip/media/js/views/forms/sections/branch-list.js
@@ -19,6 +19,8 @@ define(['jquery',
 
             this.defaults = [];
             console.log("message from branchlist.js 1");
+            console.log(this.data);
+            console.lost(this.dataKey);
             this.viewModel = JSON.parse(JSON.stringify(this.data[this.dataKey]));
             //this.viewModel.domains = this.data[this.dataKey].domains;
             this.viewModel.branch_lists = koMapping.fromJS(this.data[this.dataKey].branch_lists);
