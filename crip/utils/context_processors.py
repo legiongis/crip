cb888263ee27f9f6a6fa2d692d60f63064abe166
mradamcox <mr.adamcox@gmail.com>
2015-04-29 16:00:04 -0500

    print out content type as well

--- crip/utils/context_processors.py
+++ crip/utils/context_processors.py
@@ -62,7 +62,7 @@ def user_can_edit(request):
 def user_permissions(request):
     # need to implement proper permissions check here...
     # for now allowing all logged in users to be 'editors'
-    summary = "\n".join(["{0}|{1}|{2}".format(i.name, i.codename, i.content_type) for i in request.user.user_permissions.all()])
+    summary = "\n\r<br>".join(["{0}|{1}|{2}".format(i.name, i.codename, i.content_type) for i in request.user.user_permissions.all()])
     return {
         'user_permissions': summary
     }
