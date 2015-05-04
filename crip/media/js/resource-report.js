ba1284accf2495c9afeca72a357c36b0ff04bbd6
mradamcox <mr.adamcox@gmail.com>
2015-05-04 14:49:30 -0500

    self v this

--- crip/media/js/resource-report.js
+++ crip/media/js/resource-report.js
@@ -222,6 +222,8 @@ require([
                 });
                 //this.map.map.addLayer(this.selectedFeatureLayer);
                 position = self.map.historicLayers.length + self.map.baseLayers.length + 1;
+                console.log(position);
+                console.log(self.map.map.getLayers().length);
                 self.map.map.getLayers().insertAt(position, self.selectedFeatureLayer);                
             }
             this.selectedFeatureLayer.getSource().clear();
