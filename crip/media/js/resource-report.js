ee54be89803b0cff7895d9d58ea80f0761a22f51
mradamcox <mr.adamcox@gmail.com>
2015-05-04 13:57:05 -0500

    new way to reference map

--- crip/media/js/resource-report.js
+++ crip/media/js/resource-report.js
@@ -59,7 +59,7 @@ require([
                         source: new ol.source.GeoJSON(),
                         style: style
                     });
-                    self.map.map.addLayer(this.selectedFeatureLayer);  
+                    this.map.addLayer(this.selectedFeatureLayer);  
                 }
                 this.selectedFeatureLayer.getSource().clear();
 
