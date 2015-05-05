38a21f56338f8af3114f47fde0123424620fc5cb
mradamcox <mr.adamcox@gmail.com>
2015-05-05 18:47:34 -0500

    fix layer

--- crip/media/js/map/layers.js
+++ crip/media/js/map/layers.js
@@ -11,14 +11,16 @@ define([
         categories: ["Boundaries"],
         icon: 'fa fa-bookmark-o',
         infoContent: "this is the infoContent",
-        layer: new ol.source.TileWMS({
-            url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
-            params: {
-                'LAYERS': 'vect:fedland020',
-                'TILED': true,
-            },
-            serverType: 'geoserver'   
-        })
+        layer: new ol.layer.Tile({
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:fedland020',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'   
+            });
+        });
     }));
 
     return layers;
