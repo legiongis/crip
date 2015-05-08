d40f843505bfe2d9add0d3f54297a9e4581423e2
mradamcox <mr.adamcox@gmail.com>
2015-05-08 15:31:16 -0500

    url update

--- crip/urls.py
+++ crip/urls.py
@@ -18,6 +18,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from arches_hip import urls as arches_hip_urls
 from django.conf.urls import patterns, url, include
+from django.conf import settings
 
 uuid_regex = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
 
@@ -27,6 +28,5 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
-    from django.conf import settings
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
