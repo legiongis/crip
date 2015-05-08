4f2d2e2c03f324bb4f11ac621e907a76d921cec4
mradamcox <mr.adamcox@gmail.com>
2015-05-08 18:13:04 -0500

    verbiage updates

--- crip/media/js/map/layers.js
+++ crip/media/js/map/layers.js
@@ -10,7 +10,7 @@ define([
         name: 'Private Land in Kisatchie NF',
         categories: ["Boundaries"],
         icon: 'fa fa-bookmark-o',
-        infoContent: "This layer is provided to encourage exploration in Kisatchie while appropriately avoiding private land.  The dataset was downloaded from the <a href='http://www.fs.usda.gov/main/kisatchie/landmanagement/gis'>USFS</a> on 5-4-15.  All boundaries marked on the ground take precedence over those displayed here.",
+        infoContent: "This layer is provided to encourage responsible exploration in Kisatchie National Forest.  The dataset was downloaded from the <a href='http://www.fs.usda.gov/main/kisatchie/landmanagement/gis'>USFS</a> on 5-4-15.  All boundaries marked on the ground take precedence over those displayed on this map.",
         layer: new ol.layer.Tile({
             is_arches_layer: "nobutclose",
             source: new ol.source.TileWMS({
