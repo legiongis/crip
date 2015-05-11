028368ff91413072e802b45085165808146540f0
mradamcox <mr.adamcox@gmail.com>
2015-05-11 14:38:00 -0500

    reform function

--- crip/media/js/header.js
+++ crip/media/js/header.js
@@ -1,21 +1,24 @@
 require([
     'jquery',
     'backbone',
-], function($, Backbone, arches, ConceptModel, ConceptTree, ConceptReport, ConceptSearch, AddSchemeForm, ExportSchemeForm, DeleteSchemeForm, ImportSchemeForm) {
-    
+], function($, Backbone, arches) {
+    console.log("first print");
     function showHelp() {
         $("#help-button").click(function(){
-            
-            if ($("#help-contents").is(':visible') {
+            if (this.hasClass("help-shown")){
+                $(this).removeClass("help-shown");
+                $(this).text("SHOW HELP");
                 $("#help-contents").hide();
                 console.log("hide");
             } else {
+                $(this).addClass("help-shown");
+                $(this).text("HIDE HELP");
                 $("#help-contents").show();
                 console.log("show");
             });
         });
     };
-
+    
     $(document).ready(function() {
     
         console.log("document ready");
