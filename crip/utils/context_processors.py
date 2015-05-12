ac8a35be4523852a8fcd69358d6aa7e6c84a53ff
mradamcox <mr.adamcox@gmail.com>
2015-05-11 21:39:26 -0500

    fix url

--- crip/utils/context_processors.py
+++ crip/utils/context_processors.py
@@ -42,6 +42,7 @@ def map_info(request):
 
 def resource_types(request):
     sorted_resource_types = sorted(settings.RESOURCE_TYPE_CONFIGS().items(), key=lambda v: v[1]['sort_order'])
+    print sorted_resource_types
     return {
         'resource_types': sorted_resource_types
     }
