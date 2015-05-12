a28d590ceaf910382984f26b4b102687837db1b7
mradamcox <mr.adamcox@gmail.com>
2015-05-12 08:01:56 -0500

    add cemetery veg layers

--- crip/media/js/map/layers.js
+++ crip/media/js/map/layers.js
@@ -42,10 +42,123 @@ define([
         })
     });
     
+    var amcem_shrubs = new LayerModel({
+        name: 'American Cemetery Veg: Shrubs',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_shrubs',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    var amcem_shrubs = new LayerModel({
+        name: 'American Cemetery Veg: Ornamental Grass',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_shrubs',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    
+    var amcem_stumps = new LayerModel({
+        name: 'American Cemetery Veg: Stumps',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_stumps',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    
+    var amcem_trees = new LayerModel({
+        name: 'American Cemetery Veg: Trees',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_trees',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    
+    var amcem_vines = new LayerModel({
+        name: 'American Cemetery Veg: Vines',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_vines',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    
+    var amcem_bulbs = new LayerModel({
+        name: 'American Cemetery Veg: Bulbs',
+        categories: ["Vegetation"],
+        icon: 'fa fa-bookmark-o',
+        infoContent: "Vegetation data for the American Cemetery.  Mapped in the fall of 2014 by Laura Bradford.",
+        layer: new ol.layer.Tile({
+            is_arches_layer: "nobutclose",
+            source: new ol.source.TileWMS({
+                url: 'http://54.148.201.140:8080/geoserver/vect/wms/',
+                params: {
+                    'LAYERS': 'vect:amcem_bulbs',
+                    'TILED': true,
+                },
+                serverType: 'geoserver'
+            })
+        })
+    });
+    
     
     layers.push(
         priv_prop,
-        plss_ref
+        plss_ref,
+        amcem_bulbs,
+        amcem_grass,
+        amcem_shrubs,
+        amcem_stumps,
+        amcem_trees,
+        amcem_vines
     );
 
     return layers;
