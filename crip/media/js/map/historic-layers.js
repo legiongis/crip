6b6977b22e5feacf74d96baa26a7a5355df9405b
adam cox <mr.adamcox@gmail.com>
2015-10-03 10:32:30 -0500

    upate layer names and wms urls

--- crip/media/js/map/historic-layers.js
+++ crip/media/js/map/historic-layers.js
@@ -102,11 +102,9 @@ define([
     // full confederate map
     var confed_fullLyr = new ol.layer.Tile({
         source: new ol.source.TileWMS({
-            url: 'http://crhim.canerivernha.org:8080/geoserver/raster/wms/',
-            //url: 'http://54.148.201.140:8080/geoserver/raster/wms/',
+            url: 'http://crhim.canerivernha.org:8080/geoserver/confed_maps/wms/',
             params: {
-                //'LAYERS': 'confed_parish:sheet42_full',
-                'LAYERS': 'raster:confed_color',
+                'LAYERS': 'confed_full',
                 'TILED': true,
                 },
             serverType: 'geoserver'   
@@ -117,11 +115,9 @@ define([
     // transparent confederate map (for altlayer)
     var confed_transLyr = new ol.layer.Tile({
         source: new ol.source.TileWMS({
-            url: 'http://crhim.canerivernha.org:8080/geoserver/raster/wms/',
-            //url: 'http://54.148.201.140:8080/geoserver/raster/wms/',
+            url: 'http://crhim.canerivernha.org:8080/geoserver/confed_maps/wms/',
             params: {
-                //'LAYERS': 'confed_parish:sheet42_trans',
-                'LAYERS': 'raster:confed_trans',
+                'LAYERS': 'confed_trans',
                 'TILED': true,
                 },
             serverType: 'geoserver'   
@@ -145,7 +141,6 @@ define([
         name: "platmaps_transLyr",
         source: new ol.source.XYZ({
             url: 'http://199.184.68.66/tiles/platmaps_trans/{z}/{x}/{y}.png'
-            //url: 'https://s3-us-west-2.amazonaws.com/natchdata/tiles/historictownplats/{z}/{x}/{y}.png'
         }),
         visible: false,
     });   
@@ -155,7 +150,6 @@ define([
         name: "platmaps_fullLyr",
         source: new ol.source.XYZ({
             url: 'http://199.184.68.66/tiles/platmaps_white/{z}/{x}/{y}.png'
-            //url: 'https://s3-us-west-2.amazonaws.com/natchdata/tiles/historictownplats/{z}/{x}/{y}.png'
         }),
         visible: false,
     });
@@ -188,31 +182,6 @@ define([
         showInfo: 'This is a captured confederate map showing the road between Natchitoches and Alexandria (only the portions within Natchitoches Parish are shown here).  It was originally drawn in many segments on both sides of a single piece of paper.',
     }
     roadmap.layer.matchid = roadmap.id;
-
-    // map 174
-    var map174Lyr = new ol.layer.Tile({
-        name: "map174Lyr",
-        source: new ol.source.TileWMS({
-            //url: 'http://localhost:8080/geoserver/natchcity/wms/',
-            url: 'http://54.148.201.140:8080/geoserver/raster/wms/',
-            params: {
-                //'LAYERS': 'natchcity:map174',
-                'LAYERS': 'raster:map174',
-                'TILED': true,
-                },
-            serverType: 'geoserver'   
-        }),
-        visible: false,
-    });
-    var map174 = {
-        id: 'map174',
-        name: 'Map 174',
-        icon: arches.urls.media + 'img/map/thb_map174.png',
-        layer: map174Lyr,
-        altlayer: false,
-        showInfo: 'This is a plat map of the city of Natchitoches from 1866.  It is map number 174 at the <a href="https://library.nsula.edu/cammie-henry-research-center/" target="_blank">Cammie G. Henry Research Library</a>.',
-    }
-    map174.layer.matchid = map174.id;
 
     // aggregate layers in historicLayers array
     var historicLayers = [
